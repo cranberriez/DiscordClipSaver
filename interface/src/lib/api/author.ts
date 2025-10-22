e16434672d6a7fcff4b160ca71fb68a2f4e603aa
Jacob <jakevilevac@gmail.com>
2025-10-22 12:00:18 -0700

    feat: updated author with stats dto for client

--- interface/src/lib/api/author.ts
+++ interface/src/lib/api/author.ts
@@ -7,11 +7,11 @@
  * Used for displaying author information in clips viewer
  */
 export interface AuthorWithStats {
-    id: string;
-    username: string;
-    avatar_url: string | null;
-    clip_count: number;
-    channel_clip_counts: Record<string, number>; // channelId -> count
+    user_id: string;
+    display_name: string; // User display name, overrides username if present, overrided by nickname if present
+    avatar_url: string | null; // User avatar URL, is replaced with guild avatar URL if present
+    clip_count?: number;
+    channel_clip_counts?: Record<string, number>; // channelId -> count
 }
 
 /**
