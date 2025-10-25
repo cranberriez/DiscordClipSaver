ef1feafe19dcc88c8ff594338bb0a2d64b54ab4f
Jacob <jakevilevac@gmail.com>
2025-10-25 02:22:49 -0700

    fix: guild icon can handle full urls

--- interface/src/features/dashboard/list/components/DiscordGuildIcon.tsx
+++ interface/src/features/dashboard/list/components/DiscordGuildIcon.tsx
@@ -9,15 +9,21 @@ export function DiscordIcon({
     iconUrl: string;
     size?: "sm" | "md" | "lg";
 }) {
-    const realUrl = iconUrl
-        ? `https://cdn.discordapp.com/icons/${guildId}/${iconUrl}.png?size=64`
-        : "https://cdn.discordapp.com/embed/avatars/0.png?size=64";
-
     const sizeClasses = {
         sm: "w-10 h-10 min-w-10 min-h-10 rounded-lg",
         md: "w-16 h-16 min-w-16 min-h-16 rounded-xl",
         lg: "w-24 h-24 min-w-24 min-h-24 rounded-2xl",
     };
+
+    let realUrl = "";
+
+    if (iconUrl.includes("https://cdn.discordapp.com/icons/")) {
+        realUrl = iconUrl;
+    } else {
+        realUrl = iconUrl
+            ? `https://cdn.discordapp.com/icons/${guildId}/${iconUrl}.png?size=64`
+            : "https://cdn.discordapp.com/embed/avatars/0.png?size=64";
+    }
 
     return (
         <div className={cn("overflow-hidden aspect-square", sizeClasses[size])}>
